7f7e326b9d4b748c707ab5959d919997e8f6cf6a
Pierre - hugues Husson <husson@ours.enst.fr>
2010-10-08 00:58:41 +0200

    Add a real ATmega player

--- client/c/ships_atmega/battleship.c
+++ client/c/ships_atmega/battleship.c
@@ -1,15 +1,74 @@
-#define _GNU_SOURCE
+/*
+  bttleship.c -- simple client to play battleship for Telecom Robotics lessons.
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <strings.h>
-#include <time.h>
-#include <unistd.h>
+#include <avr/boot.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
 
-#include <connection.h>
 
-void show_usage(FILE *fd, char *name) {
-	fprintf(fd, "%s serveur_name port\n", name);
+#include "connection.h"
+#include <uart_atmega.h>
+
+#define led_rouge PD4 // rouge
+#define led_verte PD3 // vert
+
+
+// coefficient to accelerate rotation servo
+
+
+// manipulation des LEDs
+#define turn_off_led_rouge()  PORTD&=~(1<<led_rouge)
+#define turn_off_led_verte()  PORTD&=~(1<<led_verte)
+#define turn_on_led_rouge()  PORTD|=(1<<led_rouge)
+#define turn_on_led_verte()  PORTD|=(1<<led_verte)
+#define toggle_led_rouge()  PORTD^=(1<<led_rouge)
+#define toggle_led_verte()  PORTD^=(1<<led_verte)
+
+char *index2(char *val, int c) {
+	int i=0;
+	while(val[i]!=c) ++i;
+	return val+i;
+}
+
+void wait_real_ms(short time) {
+	short i, j;
+	for (j=0; j<time; j++) {
+		// on a besoin de 7 instructions pour une addition sur des shorts
+		// le quartz est à 12MHz
+		for (i=0; i<(12000/7); i++)
+		{
+			asm("nop");
+		}
+	}
+}
+
+
+
+void init_leds(void) {
+	//On met les pins sur lesquelles sont branchées les diodes en output
+	DDRD |= _BV(led_verte) | _BV(led_rouge);
+}
+
+
+// allumer tout pendant 1 sec
+void test_leds(void) {
+	turn_on_led_rouge();
+	turn_on_led_verte();
+	wait_real_ms(1000);
+	turn_off_led_rouge();
+	turn_off_led_verte();
+}
+
+// eteindre les 4 leds
+void eteindre_leds (void)
+{
+	turn_off_led_rouge();
+	turn_off_led_verte();
 }
 
 void send_str(struct connection *cn_hdl, char *str) {
@@ -19,7 +78,7 @@ void send_str(struct connection *cn_hdl, char *str) {
 char *recv_str(struct connection *cn_hdl) {
 	static char str[128];
 	// 128B should be enough for everyone
-	bzero(str, 128);
+	memset(str, 0, 128);
 	recv_message(cn_hdl, str, 128);
 	return str;
 }
@@ -41,7 +100,6 @@ int place(int x, int y, int orientation/* 0 ==H; 1==V*/, int length) {
 				for(j=0;j<i;++j) {
 					map[x][y+j]=0;
 				}
-				printf("Conflict found\n");
 				return 0;
 			}
 			map[x][y+i]=1;
@@ -54,7 +112,6 @@ int place(int x, int y, int orientation/* 0 ==H; 1==V*/, int length) {
 				for(j=0;j<i;++j) {
 					map[x+j][y]=0;
 				}
-				printf("Conflict found\n");
 				return 0;
 			}
 			map[x+i][y]=1;
@@ -63,30 +120,31 @@ int place(int x, int y, int orientation/* 0 ==H; 1==V*/, int length) {
 	return 1;
 }
 
-int main (int argc, char *argv[]) {
+int main(void)
+{
+	  init_leds();
+	  test_leds();
+	  wait_real_ms(1000);
+	  test_leds();
 	struct connection *my_connection;
-	int port;
-	char *server_name;
 	char cmd[128];
+	memset(cmd, 0, 128);
 
-	srand(time(NULL));
-	printf("Welcome to Battleship!\n");
+	srand(42);
 
-	if (argc != 3) {
-		show_usage(stderr, argv[0]);
-		return EXIT_FAILURE;
-	}
-
-	server_name = argv[1];
-	port = atoi(argv[2]);
-
-	my_connection = open_connection(port, server_name);
-	if (my_connection == NULL) {
-		fprintf(stderr, "Problem while opening connection on %s:%d\n", server_name, port);
-		return EXIT_FAILURE;
-	}
+	my_connection = open_connection(0, NULL);
 
 	char *val;
+	{
+		uart_puts("+++");
+		wait_real_ms(1100);
+		uart_puts("ATCH14\r\n");
+		uart_puts("atid5353\r\n");
+		uart_puts("atdlCC\r\n");
+		uart_puts("atmyPH\r\n");
+		uart_puts("ATCN\r\n");
+	}
+	send_str(my_connection, "\n");
 	send_str(my_connection, "NEWGAME\n");
 	{
 		int length;
@@ -94,16 +152,7 @@ int main (int argc, char *argv[]) {
 		int i;
 		int placed;
 		for(i=0;i<10;++i)
-			bzero(map[i], 10);
-				{
-					int x,y;
-					for(x=0;x<10;++x) {
-						for(y=0;y<10;++y)
-							printf("%d ", map[x][y]);
-						printf("\n");
-					}
-				}
-				printf("\n\n");
+			memset(map[i], 0, 10);
 		while(1) {
 			placed=0;
 			val=recv_str(my_connection);
@@ -111,12 +160,10 @@ int main (int argc, char *argv[]) {
 			if(strncmp("FIRE", val, 4)==0)
 				break;
 			if(strncmp("SHIP;", val, 5)!=0) {
-				fprintf(stderr, "Wrong protocol, byebye (%s:%s:%d)\n", __FILE__, __FUNCTION__, __LINE__);
 				exit(-1);
 			}
 			length=atoi(val+5);
 			if(length<2 || length>5) {
-				fprintf(stderr, "Wrong protocol, byebye (%s:%s:%d)\n", __FILE__, __FUNCTION__, __LINE__);
 				exit(-1);
 			}
 			while(!placed) {
@@ -125,36 +172,24 @@ int main (int argc, char *argv[]) {
 				if(x+(length)>=10 && y+(length)>=10) {
 					//We're at bottom right
 					x-=length;
-					asprintf(&cmd, "SHIP;%d;%d;H\n", x, y);
+					snprintf(cmd, 128, "SHIP;%d;%d;H\n", x, y);
 					placed=place(x,y, 0, length);
-					printf("\t0\n");
 				} else if(x+(length)>=10) {
-					asprintf(&cmd, "SHIP;%d;%d;V\n", x, y);
+					snprintf(cmd, 128, "SHIP;%d;%d;V\n", x, y);
 					placed=place(x,y, 1, length);
-					printf("\t1\n");
 				} else if(y+(length)>=10) {
-					asprintf(&cmd, "SHIP;%d;%d;H\n", x, y);
+					snprintf(cmd, 128, "SHIP;%d;%d;H\n", x, y);
 					place(x,y, 1, length);
-					printf("\t2\n");
 				} else {
 					i=rand()%2;
-					asprintf(&cmd, "SHIP;%d;%d;%c\n", x, y, i ? 'H' : 'V');
+					snprintf(cmd, 128, "SHIP;%d;%d;%c\n", x, y, i ? 'H' : 'V');
 					placed=place(x,y, !i, length);
-					printf("\t3\n");
 				}
 			}
 			send_str(my_connection, cmd);
 			val=recv_str(my_connection);
 			if(strncmp(val, "OK", 2)!=0) {
 				fprintf(stderr, "Fail to place ships\n");
-				{
-					int x,y;
-					for(x=0;x<10;++x) {
-						for(y=0;y<10;++y)
-							printf("%d ", map[x][y]);
-						printf("\n");
-					}
-				}
 				return -1;
 			}
 		}
@@ -165,14 +200,13 @@ int main (int argc, char *argv[]) {
 	//Now let's play.
 	{
 		int i;
-		int x,y,j,last_x,last_y;
+		int x,y,j,last_x=0,last_y=0;
 		for(i=0;i<10;++i)
-			bzero(map[i], 10);
+			memset(map[i], 0, 10);
 		j=0;
 		int state=0;
 		while(1) {
 			++j;
-			printf("j=%d\n", j);
 			if(j==100)
 				exit(-2);
 			if(!state) {
@@ -198,19 +232,16 @@ int main (int argc, char *argv[]) {
 				} while(map[x][y] || (x%2 == y%2) );
 #endif
 			} else {
-				printf("Hahahaa!\n");
 				int got=0;
 				//Test au dessus
 				i=0;
 				x=last_x;
 				y=last_y;
-				printf("(%d):%d:%d:%d\n", __LINE__, x, y, got);
 				while(map[x][y+i]==1 && y+i<9) ++i;
 				if(map[x][y+i]==0) {
 					y+=i;
 					got=1;
 				}
-				printf("(%d):%d:%d:%d\n", __LINE__, x, y, got);
 				//Test au dessous
 				if(!got) {
 					i=0;
@@ -220,7 +251,6 @@ int main (int argc, char *argv[]) {
 						got=1;
 					}
 				}
-				printf("(%d):%d:%d:%d\n", __LINE__, x, y, got);
 				//Test Ã  droite
 				if(!got) {
 					i=0;
@@ -230,7 +260,6 @@ int main (int argc, char *argv[]) {
 						got=1;
 					}
 				}
-				printf("(%d):%d:%d:%d\n", __LINE__, x, y, got);
 				//Test Ã  gauche
 				if(!got) {
 					i=0;
@@ -240,18 +269,7 @@ int main (int argc, char *argv[]) {
 						got=1;
 					}
 				}
-				printf("(%d):%d:%d:%d\n", __LINE__, x, y, got);
 				if(!got) {
-					printf("Agaaaaaaaaa?\n");
-					{
-						int x,y;
-						for(x=0;x<10;++x) {
-							for(y=0;y<10;++y)
-								printf("%d ", map[x][y]);
-							printf("\n");
-						}
-						printf("last_x=%d,last_y=%d\n", last_x, last_y);
-					}
 					state=0;
 					do {
 						x=rand()%10;
@@ -259,17 +277,14 @@ int main (int argc, char *argv[]) {
 					} while(map[x][y]);
 				}
 			}
-			asprintf(&cmd, "FIRE;%d;%d\n", x, y);
-			printf("%s", cmd);
+			snprintf(cmd, 128, "FIRE;%d;%d\n", x, y);
 			send_str(my_connection, cmd);
 			{
 				int next=0;
 				while(!next) {
 					val=recv_str(my_connection);
-					printf("%s\n", val);
 					while(val && val[0]) {
 						if(strncmp(val, "TOUCH", 5)==0) {
-							printf("%dx%d=1\n", x, y);
 							map[x][y]=1;
 							if(strncmp(val, "TOUCHE-COULE", 9)==0) {
 								state=0;
@@ -279,19 +294,17 @@ int main (int argc, char *argv[]) {
 								last_y=y;
 							}
 						} else if(strncmp(val, "COUL", 4)==0 || strncmp(val, "RATE", 4)==0) {
-							printf("%dx%d=2\n", x, y);
 							map[x][y]=2;
 						} else if(strncmp(val, "YOU ", 4)==0) {
-							printf("FINI(%s)\n", val);
 							return -1;
 						} else if(strncmp(val, "FIRE", 4)==0) {
 							next=1;
 						} else if(strncmp(val, "OK", 2)==0) {
 							//Non rien...
 						} else {
-							printf("Unknown val:'%s'(%p)={%d}\n", val, val, val[0]);
+							//Ouais bon on se suicide mais non.
 						}
-						val=index(val, '\n');
+						val=index2(val, '\n');
 						if(val)
 							val++;
 					}
@@ -300,7 +313,6 @@ int main (int argc, char *argv[]) {
 		}
 
 	}
-
-	printf("End of Batlleship...\n");
-	return EXIT_SUCCESS;
+	return 0;
 }
+
